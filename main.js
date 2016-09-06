cd53800d58224ba7e09ea3c699e481703150b1c5
Vladimir Kamuz <v.kamuz@gmail.com>
2016-09-06 22:27:43 +0300

    Making requests asynchronous

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 var request = new XMLHttpRequest();
-request.open('GET', 'data.txt', false);
+request.open('GET', 'data.txt');
+request.onreadystatechange = function(){
+  console.log(request.readyState);
+  if ((request.readyState === 4) && (request.status === 200)){
+    console.log(request);
+    document.writeln(request.responseText);
+  }
+}
 request.send();
-console.log(request);
